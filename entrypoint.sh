fb6601ae5095fd71f322347e7a1e6defc91ce93c
aiziyuer <ziyu0123456789@gmail.com>
2021-03-28 11:38:54 -0400

    修复入口脚本错误

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ if [ "$1" = 'server' ]; then
 
 WATCH=${WATCH:-'.'}
 fsnotify-exec -w $WATCH <<'EOF'
-find /etc/fsnotify.d/ -name *.sh | xargs -n1 -I{} sh -c "echo {} && sh {}"
+find /etc/fsnotify.d/ -name '*.sh' | xargs -n1 -I{} sh -c "echo {} && sh {}"
 EOF
 
 else
